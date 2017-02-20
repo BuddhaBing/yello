d90fbf03f355e03b3d62fb07eece0377449fc9b7
Benjamin Carson <benjohncarson@gmail.com>
2017-02-20 14:49:44 +0000

    new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,27 @@
 class RestaurantsController < ApplicationController
+  
+  include Rails.application.routes.url_helpers
+  
+  def self.default_url_options
+    ActionMailer::Base.default_url_options
+  end
+  
   def index
     @restaurants = Restaurant.all
   end
-
+  
+  def new
+    @restaurant = Restaurant.new
+  end
+  
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+  
+  private
+  
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  # Url helpers- more research to implemenet
+  #   extend ActiveSupport::Concern
+  # include Rails.application.routes.url_helpers
+
+  # included do
+  #   def default_url_options
+  #     ActionMailer::Base.default_url_options
+  #   end
+  # end
 end
+
+# class UrlGenerator
+#   include ApplicationHelper
+# end
+
+# generator = UrlGenerator.new
+# generator.restaurants_url
+# generator.restaurants_path
+# generator.new_restaurant_url
+# generator.new_restaurant_path
+  
