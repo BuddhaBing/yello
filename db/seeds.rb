04e5493c96cd2dd8e3cc2e42bfb4e6c68341e753
Benjamin Carson <benjohncarson@gmail.com>
2017-02-21 15:24:51 +0000

    permit username, added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.delete_all
+
+User.create! :user_name => 'John Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :user_name => 'Ivan Drago', :email => 'ivan@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+User.create! :user_name => 'Rocky', :email => 'rocky@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+
+Restaurant.delete_all
+
+Restaurant.create!(name: "Nandos", description: "Cheeky")
+Restaurant.create!(name: "KFC", description: "Dirty")
+Restaurant.create!(name: "Wimpy", description: "Filthy")
