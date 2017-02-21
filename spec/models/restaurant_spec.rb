4fd76720427694fc263fd8acf67bf5dee0411d64
Joe Marriott <marriott90@gmail.com>
2017-02-21 14:15:25 +0000

    tidying up

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   it "is not valid with a name of less than 3 chars" do
     restaurant = Restaurant.new(name:"na")
     expect(restaurant).to have(1).error_on(:name)
