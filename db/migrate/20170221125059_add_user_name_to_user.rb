19a1dae6a6cc9e613a2d7360c79caf2941b13d32
James Turner <mail.jt@gmail.com>
2017-02-21 14:29:21 +0000

    added username

--- db/migrate/20170221125059_add_user_name_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddUserNameToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :username, :string
-    add_index :users, :username, unique: true
-    add_column :users, :username, null: false
-  end
-end
