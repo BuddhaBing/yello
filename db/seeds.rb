3a4fff1f38d5e8fd36cb51625fed047640efb00a
James Turner <mail.jt@gmail.com>
2017-02-22 15:34:29 +0000

    tidied_seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Review.delete_all
-Restaurant.delete_all
-User.delete_all
+User.create user_name: 'John Doe'  , email: 'john@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
+User.create user_name: 'Ivan Drago', email: 'ivan@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
+User.create user_name: 'Rocky'     , email: 'rocky@example.com', password: 'topsecret', password_confirmation: 'topsecret'
 
-User.create! :user_name => 'John Doe', :email => 'john@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
-User.create! :user_name => 'Ivan Drago', :email => 'ivan@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
-User.create! :user_name => 'Rocky', :email => 'rocky@gmail.com', :password => 'topsecret', :password_confirmation => 'topsecret'
+Restaurant.create(name: "Nandos", description: "Cheeky")
+Restaurant.create(name: "KFC",    description: "Dirty")
+Restaurant.create(name: "Wimpy",  description: "Filthy")
 
-Restaurant.create!(name: "Nandos", description: "Cheeky")
-Restaurant.create!(name: "KFC", description: "Dirty")
-Restaurant.create!(name: "Wimpy", description: "Filthy")
-
-Review.create!(thoughts: "nasty" , rating: 1, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
-Review.create!(thoughts: "smelly", rating: 4, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
-Review.create!(thoughts: "wut"   , rating: 3, restaurant_id: Restaurant.find_by(name: 'Wimpy').id)
+Review.create(thoughts: "nasty" , rating: 1, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
+Review.create(thoughts: "smelly", rating: 4, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
+Review.create(thoughts: "wut"   , rating: 3, restaurant_id: Restaurant.find_by(name: 'Wimpy').id)
