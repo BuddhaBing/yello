6c65e56bad824973a2374ff494f58226e435bf01
James Turner <mail.jt@gmail.com>
2017-02-22 12:25:58 +0000

    maybe working?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221153419) do
+ActiveRecord::Schema.define(version: 20170222113430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20170221153419) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "user_name",                           null: false
+    t.string   "providers"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
