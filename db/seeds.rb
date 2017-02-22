3c90b277cddd7acce549657d3ab792aefa140fae
Rob Brentnall <rob.brentnall@gmail.com>
2017-02-22 16:05:53 +0000

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-User.create user_name: 'John Doe'  , email: 'john@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
-User.create user_name: 'Ivan Drago', email: 'ivan@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
-User.create user_name: 'Rocky'     , email: 'rocky@example.com', password: 'topsecret', password_confirmation: 'topsecret'
+user1 = User.create user_name: 'John Doe'  , email: 'john@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
+user2 = User.create user_name: 'Ivan Drago', email: 'ivan@example.com',  password: 'topsecret', password_confirmation: 'topsecret'
+user3 = User.create user_name: 'Rocky'     , email: 'rocky@example.com', password: 'topsecret', password_confirmation: 'topsecret'
 
-Restaurant.create(name: "Nandos", description: "Cheeky")
-Restaurant.create(name: "KFC",    description: "Dirty")
-Restaurant.create(name: "Wimpy",  description: "Filthy")
+restaurant1 = Restaurant.create(name: "Nandos", description: "Cheeky", user_id: user1.id)
+restaurant2 = Restaurant.create(name: "KFC",    description: "Dirty", user_id: user2.id)
+restaurant3 = Restaurant.create(name: "Wimpy",  description: "Filthy", user_id: user3.id)
 
-Review.create(thoughts: "nasty" , rating: 1, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
-Review.create(thoughts: "smelly", rating: 4, restaurant_id: Restaurant.find_by(name: 'Nandos').id)
-Review.create(thoughts: "wut"   , rating: 3, restaurant_id: Restaurant.find_by(name: 'Wimpy').id)
+Review.create(thoughts: "nasty" , rating: 1, restaurant_id: restaurant1.id)
+Review.create(thoughts: "smelly", rating: 4, restaurant_id: restaurant2.id)
+Review.create(thoughts: "wut"   , rating: 3, restaurant_id: restaurant3.id)
